0a7220cf5d38e2a8e7efdb98526217a3d07de792
bronko <bronko79@users.noreply.github.com>
2025-08-12 14:54:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -312,7 +312,7 @@ class TastytradeIngestor:
           month=now.month,
           day=now.day,
           hour=12,  # Stunden
-          minute=54,  # Minuten
+          minute=56,  # Minuten
           second=0,
           microsecond=0
       )
@@ -487,6 +487,7 @@ if __name__ == "__main__":
     
 
 """
+
 
 
 
